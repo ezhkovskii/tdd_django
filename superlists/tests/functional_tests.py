65caedcd5919cf601ebe3f54a361f7e807cf461f
ezhkov <ezhkovskii@gmail.com>
2022-08-10 00:58:27 +0500

    chapter 4.1

--- superlists/tests/functional_tests.py
+++ superlists/tests/functional_tests.py
@@ -1,7 +1,11 @@
+import time
 import unittest
 
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
+
 
 
 class NewVisitorTest(unittest.TestCase):
@@ -11,21 +15,28 @@ class NewVisitorTest(unittest.TestCase):
         """установка"""
         self.browser = webdriver.Chrome(ChromeDriverManager().install())
 
+
     def tearDown(self):
         """закрытие"""
         self.browser.quit()
 
     def test_can_start_a_list_and_retrieve_it_later(self):
-        """тест: можно начать список и получить его позже"""
-        # Эдит слышала про крутое новое онлайн-приложение со
-        # списком неотложных дел. Она решает оценить его
-        # домашнюю страницу
+        '''тест: можно начать список и получить его позже'''
         self.browser.get('http://localhost:8000')
-        # Она видит, что заголовок и шапка страницы говорят о
-        # списках неотложных дел
         self.assertIn('To-Do', self.browser.title)
+        header_text = self.browser.find_element(By.TAG_NAME, 'h1').text
+        self.assertIn('To-Do', header_text)
+        inputbox = self.browser.find_element(By.ID, 'id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
+        inputbox.send_keys('Купить павлиньи перья')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+        table = self.browser.find_element(By.ID, 'id_list_table')
+        rows = table.find_elements(By.TAG_NAME, 'tr')
         self.fail('Закончить тест!')
-        # Ей сразу же предлагается ввести элемент списка
 
 
 if __name__ == '__main__':
